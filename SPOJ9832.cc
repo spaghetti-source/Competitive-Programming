77fd8288b9e8709d46acc5acd3e1e79349c85c97
Takanori MAEHARA <maehara@prefield.com>
2015-01-27 10:57:29 +0900

    SPOJ 9832: Matrix Inverse

--- SPOJ9832.cc
+++ SPOJ9832.cc
@@ -28,14 +28,9 @@ ll add(ll a, ll b, ll M) {
   return a;
 }
 ll mul(ll a, ll b, ll M) {
-  ll ans = (a * b) % M;
-  if (ans < 0) ans += M;
-  return ans;
-  /*
   ll r = a*b - ll(double(a)*b/M + 0.5)*M;
   while (r < 0) r += M;
   return r;
-  */
 }
 ll div(ll a, ll b, ll M) {
   ll u = 1, x = 0, s = b, t = M;
@@ -47,42 +42,6 @@ ll div(ll a, ll b, ll M) {
   if (a % t) return -1;
   return mul(x < 0 ? x + M : x, a / t, M);
 }
-
-
-template <class T>
-ostream &operator<<(ostream &os, const vector<T> &v) {
-  os << "[";
-  for (int i = 0; i < v.size(); os << v[i++]) 
-    if (i > 0) os << " ";
-  os << "]";
-  return os;
-}
-
-
-// solve A x = b in mod M transform A into a Smith normal form
-/*
-vec solve(mat A, vec b, ll M) {
-  int n = A.size();
-  for (int i = 0; i < n; ++i) {
-    for (int j = i+1; j < n; ++j) {
-      while (A[j][i] != 0) {
-        ll t = A[i][i] / A[j][i];
-        for (int k = i; k < n; ++k) 
-          swap(A[i][k] -= t * A[j][k], A[j][k]);
-        swap(b[i] -= t * b[j], b[j]);
-      }
-    }
-  }
-  for (int i = n-1; i >= 0; --i) {
-    for (int j = i+1; j < n; ++j) 
-      b[i] = sub(b[i], mul(A[i][j], b[j], M), M);
-    if ((b[i] = div(b[i], A[i][i], M)) < 0) return {};
-  }
-  return b;
-}
-*/
-
-
 
 mat inverse(mat A, ll M) {
   int n = A.size();
